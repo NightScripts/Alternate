3685600b8725a0f9343b047c5f70c74c2fad7c3c
Night <166496112+N1GHTLY@users.noreply.github.com>
2024-07-07 01:40:21 +0800

    Create config.lua
    Config for Alternate

---
+++ config.lua
@@ -0,0 +1,25 @@
+getgenv().Alternate = {
+  ["Auto Parry"] = {
+    ["State"] = true,
+    ["Mode"] = "Fierce"
+  },
+  ["Auto Spam"] = {
+    ["State"] = true,
+    ["Mode"] = "Flash",
+    ["Distance"] = {10, 5}
+  },
+  ["Anti Curve"] = true,
+  ["Visualizer"] = {
+    ["Size"] = 10,
+    ["Shape"] = "Sphere"
+  },
+  ["Anti Ability"] = {"Pull", "Infinity", "Forcefield"},
+  ["Mod Detect"] = true,
+  ["Anti Lag"] = true,
+  ["Effects"] = {
+    ["Slash Effect"] = true,
+    ["Sound Effect"] = true,
+    ["Slash EFX"] = "default",
+    ["Sound ID"] = 0
+  }
+}
